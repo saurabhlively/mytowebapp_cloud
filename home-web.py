eb474ed94b59fc42441f95c3b1beaba866c9ee0f
saurabh <saurabhlively@yahoo.co.in>
2024-11-17 14:01:37 +0530

    Multipages app in one code(Created about.py as well a page)

--- home-web.py
+++ home-web.py
@@ -5,6 +5,8 @@ import os
 if not os.path.exists('todos.txt'):
     with open('todos.txt','w') as file:
         pass
+st.set_page_config(layout='wide')
+
 
 todos=[]
 def add_todo():
@@ -19,7 +21,7 @@ todos=functions.get_todos()
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
-st.write("This app is to increase your productivity.")
+st.write("<h1>This app is to increase your <b>productivity</b>.</h1>",unsafe_allow_html=True)
 
 """Below code is for removing item from ui which is checked"""
 for index,todo in enumerate(todos):
